684e9119a78f0f04d2f6b26f668ee13c5127be46
ssw <771020712@qq.com>
2024-01-28 16:59:38 +0800

    backup.2022.04.25

--- openwrt_flippy.sh
+++ openwrt_flippy.sh
@@ -3,17 +3,17 @@
 echo "package by openwrt_sswdr.backup.2022.04.25.sh"
 
 sudo apt-get update && sudo apt-get install -y p7zip p7zip-full zip unzip gzip xz-utils pigz zstd
-
 git clone https://github.com/rin0612/openwrt_packit -b backup.2022.04.25 /opt/openwrt_packit
 cd /opt/openwrt_packit
 wget -q ${OPENWRT_ARMVIRT}
 
-mkdir kernel
-# wget -q -O /opt/openwrt_packit/kernel/kernel.tar.gz https://github.com/rin0612/openwrt_packit/releases/download/backup.kernel/5.10.110.tar.gz
-wget -q -O /opt/openwrt_packit/kernel/kernel.tar.gz ${KERNEL_REPO_URL}/${KERNEL_VERSION}.tar.gz
-tar -zxvf /opt/openwrt_packit/kernel/kernel.tar.gz && rm /opt/openwrt_packit/kernel/kernel.tar.gz
+mkdir kernel && cd kernel
+# wget -q -O kernel.tar.gz https://github.com/rin0612/openwrt_packit/releases/download/backup.kernel/5.10.110.tar.gz
+wget -q -O kernel.tar.gz ${KERNEL_REPO_URL}/${KERNEL_VERSION}.tar.gz
+tar -zxvf kernel.tar.gz && rm kernel.tar.gz
 
-cat << EOF >> /opt/openwrt_packit/make.env
+cd /opt/openwrt_packit
+cat << EOF >> make.env
 WHOAMI="${WHOAMI}"
 KERNEL_VERSION="{KERNEL_VERSION_NAME}"
 KERNEL_PKG_HOME="/opt/openwrt_packit/kernel"
