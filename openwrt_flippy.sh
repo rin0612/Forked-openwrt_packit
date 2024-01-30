d78009b173bdb84341cc3a54b59957fc0c14fb41
ssw <771020712@qq.com>
2024-01-30 23:19:26 +0800

    backup.2022.04.25

--- openwrt_flippy.sh
+++ openwrt_flippy.sh
@@ -35,6 +35,7 @@ echo "------ 打包完成"
 cd /opt/openwrt_packit/output
 gzip ./*.img
 mv ../*.tar.gz ./
+echo "------ 准备打包信息到github环境变量方便后续使用"
 echo "PACKAGED_OUTPUTDATE=$(date +"%Y.%m.%d.%H%M")" >>$GITHUB_ENV
 echo "PACKAGED_OUTPUTPATH=${PWD}" >>$GITHUB_ENV
 echo "------ 打包信息: PACKAGED_OUTPUTDATE=$(date +"%Y.%m.%d.%H%M")"
